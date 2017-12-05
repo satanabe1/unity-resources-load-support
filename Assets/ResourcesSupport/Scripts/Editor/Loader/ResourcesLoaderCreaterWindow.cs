32eb9f69d1cf5c112a97a7ed9466b65e9510b4ed
Shinogi Kazuki <shinogi_kazuki@cyberagent.co.jp>
2017-12-05 22:38:41 +0900

    add Window

---
+++ Assets/ResourcesSupport/Scripts/Editor/Loader/ResourcesLoaderCreaterWindow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ResourcesSupport
+{
+    public class ResourcesLoaderCreaterWindow : EditorWindow
+    {
+        public static void Open()
+        {
+            GetWindow<ResourcesLoaderCreaterWindow>();
+        }
+
+        private ResourcesLoaderSetting setting;
+
+        void OnGUI()
+        {
+            setting = EditorGUILayout.ObjectField(setting, typeof(ResourcesLoaderSetting), false) as ResourcesLoaderSetting;
+            if (setting == null)
+            {
+                return;
+            }
+
+            if (GUILayout.Button("生成"))
+            {
+                ResourcesLoaderCreater.Create(setting);
+            }
+        }
+    }
+}
---
+++ Assets/ResourcesSupport/Scripts/Editor/ResourcesSupportMenuItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+namespace ResourcesSupport
+{
+    public class ResourcesLoaderMenuItem
+    {
+        private const string Prefix = "ResourcesSupport/";
+
+        [MenuItem(Prefix + "Open Create Loader Window")]
+        public static void CreateResourcesLoader()
+        {
+            ResourcesLoaderCreaterWindow.Open();
+        }
+
+        [MenuItem(Prefix + "Create Setting")]
+        public static void CreateResourcesLoaderSetting()
+        {
+            CreateScriptableObject<ResourcesLoaderSetting>("Assets");
+        }
+
+        [MenuItem(Prefix + "Create Load Parameter")]
+        public static void CreateLoadParameter()
+        {
+            CreateScriptableObject<LoadParameter>("Assets");
+        }
+
+        private static void CreateScriptableObject<T>(string dir) where T : ScriptableObject
+        {
+            var obj = ScriptableObject.CreateInstance<T>();
+            ProjectWindowUtil.CreateAsset(obj, GetCreateScriptableObjectPath<T>(dir));
+            AssetDatabase.Refresh();
+        }
+
+        private static string GetCreateScriptableObjectPath<T>(string dir)
+        {
+            var pathWithoutExtension = Path.Combine(dir, typeof(T).Name);
+            var path = string.Format("{0}.asset", pathWithoutExtension);
+            return AssetDatabase.GenerateUniqueAssetPath (path);
+        }
+    }
+}
